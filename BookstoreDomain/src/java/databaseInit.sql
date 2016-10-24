cc3bcc71299d57a32fe430f5afb8eda1b80550e3
dangcuuson <dangcuuson@mail.com>
2016-10-24 19:48:28 +1100

    fix databaseInit.sql

--- BookstoreDomain/src/java/databaseInit.sql
+++ BookstoreDomain/src/java/databaseInit.sql
@@ -1,8 +1,10 @@
 # Set up init data when the application is deployed
 
 # Set up role
-insert into BookstoreRole(Id, RoleName, username) values (1, 'ADMIN');
-insert into BookstoreRole(Id, RoleName, username) values (2, 'USER');
+insert into BookstoreRole(Id, RoleName) values (1, 'ADMIN')
+insert into BookstoreRole(Id, RoleName) values (2, 'USER')
+insert into BookstoreRole(Id, RoleName) values (3, 'INACTIVATED')
+insert into BookstoreRole(Id, RoleName) values (4, 'BANNED')
 
 # Set up views for jdbc realm
 create view jdbcrealm_user (username, password) as
@@ -18,5 +20,5 @@ on u.role_id = r.id;
 
 # Generate user
 # default password: qwerty
-insert into BookstoreUser(USERNAME, PASSWORD, ROLE_ID, FULLNAME) values ('admin', '65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5', 1, 'Administrator');
-insert into BookstoreUser(USERNAME, PASSWORD, ROLE_ID, FULLNAME) values ('sondang2412', '65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5', 2, 'Cuu Son Dang');
+insert into BookstoreUser(USERNAME, PASSWORD, ROLE_ID, FULLNAME) values ('admin', '65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5', 1, 'Administrator')
+insert into BookstoreUser(USERNAME, PASSWORD, ROLE_ID, FULLNAME) values ('sondang2412', '65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5', 2, 'Cuu Son Dang')
