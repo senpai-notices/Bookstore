29a30a18e67af08fdbc2bc9d1e5c5434d26bffc7
liuxy900211 <liuxy900211@172.16.53.169>
2016-10-25 11:32:46 +0800

    #: Updated the sql file, fixed a bug -author:12425664

--- BookstoreDomain/src/java/databaseInit.sql
+++ BookstoreDomain/src/java/databaseInit.sql
@@ -7,18 +7,13 @@ insert into BookstoreRole(Id, RoleName) values (3, 'INACTIVATED')
 insert into BookstoreRole(Id, RoleName) values (4, 'BANNED')
 
 # Set up views for jdbc realm
-create view jdbcrealm_user (username, password) as
-select username, password
-from BookstoreUser;
+create view jdbcrealm_user (username, password) as select username, password from BookstoreUser
 
-create view jdbcrealm_group (username, groupname) as
-select u.username, r.roleName
-from BookstoreUser u left join BookstoreRole r
-on u.role_id = r.id;
+create view jdbcrealm_group (username, groupname) as select u.username, r.roleName from BookstoreUser u left join BookstoreRole r on u.role_id = r.id
 
 # Generate test data
 
 # Generate user
 # default password: qwerty
-insert into BookstoreUser(USERNAME, PASSWORD, ROLE_ID, FULLNAME) values ('admin', '65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5', 1, 'Administrator')
-insert into BookstoreUser(USERNAME, PASSWORD, ROLE_ID, FULLNAME) values ('sondang2412', '65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5', 2, 'Cuu Son Dang')
+insert into BookstoreUser(EMAIL,USERNAME, PASSWORD, ROLE_ID, FULLNAME) values ('uts0816@gmail.com','admin', '65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5', 1, 'Administrator')
+insert into BookstoreUser(EMAIL, USERNAME, PASSWORD, ROLE_ID, FULLNAME) values ('sondang2412@gmail.com','sondang2412', '65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5', 2, 'Cuu Son Dang')
