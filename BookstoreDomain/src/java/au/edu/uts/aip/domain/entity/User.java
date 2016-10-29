1555aa983d82e032e3c164823014c9199f9fd9d5
liuxy900211 <liuxy900211@172.16.53.169>
2016-10-29 15:53:51 +0800

    #added the related comments to the code in User model class

--- BookstoreDomain/src/java/au/edu/uts/aip/domain/entity/User.java
+++ BookstoreDomain/src/java/au/edu/uts/aip/domain/entity/User.java
@@ -1,5 +1,6 @@
 package au.edu.uts.aip.domain.entity;
 
+/*import the needed libraries*/
 import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -9,10 +10,26 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+
+/**
+ * A Data model class to access data in the BOOKSTOREUSER table.
+ * User describes the a bookstore web site user
+ * It has properties Username, Fullname, Password, Email and Role
+ * All the attributes of this model class are private attributes 
+ * and all of them have relative get and set functions in order to access the attributes
+ * 
+ * @author 
+ */
 @Entity
 @Table(name = "BookstoreUser")
 public class User implements Serializable {
     
+    
+    /**
+     * The definition of the attributes of User Model class
+     * The pattern is governed by regular expression
+     * And the size are defined
+    */
     @NotNull
     @Size(min = 6, max = 32)
     @Pattern(regexp = "^[a-zA-Z0-9_]*$")
@@ -32,16 +49,28 @@ public class User implements Serializable {
     private String email;
     
     private Role role;
-
+     /**
+     * The username of the web site user in Bookstoreuser table
+     * Not null value
+     * the length of the string of username is >=6 && <=32
+     * @return a string
+     */
     @Id
     public String getUsername() {
         return username;
     }
-
+    
+     
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * The full name of the web site user in Bookstoreuser table
+     * the length of the string of fullname is less than or equal to 255
+     * Not null value
+     * @return a string
+     */
     public String getFullname() {
         return fullname;
     }
@@ -50,6 +79,12 @@ public class User implements Serializable {
         this.fullname = fullname;
     }
 
+    /**
+     * The password of the web site user in Bookstoreuser table
+     * the minimum length of the password is 6
+     * Not null value
+     * @return a String 
+     */
     public String getPassword() {
         return password;
     }
@@ -58,6 +93,12 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    
+    /**
+     * The role of the web site user in Bookstoreuser table
+     * Not null value
+     * @return a Role object
+     */
     @ManyToOne
     public Role getRole() {
         return role;
@@ -67,6 +108,11 @@ public class User implements Serializable {
         this.role = role;
     }
 
+    /**
+     * The email of the web site user in Bookstoreuser table
+     * Not null value. The format of value should abide by the format of emails. i.e. aaa@gmail.com
+     * @return a string
+     */
     public String getEmail() {
         return email;
     }
