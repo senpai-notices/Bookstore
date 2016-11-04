2be5f7f3a9c1024bd179475bf62a4cae69e3811c
tanman- <tanman-@users.noreply.github.com>
2016-11-04 20:03:48 +1100

    Add optional fields

--- BookstoreDomain/src/java/au/edu/uts/aip/domain/pin/dto/PinCard.java
+++ BookstoreDomain/src/java/au/edu/uts/aip/domain/pin/dto/PinCard.java
@@ -12,7 +12,10 @@ public class PinCard implements Serializable {
     private String cvc;
     private String name;
     private String addressLine1;
+    private String addressLine2;
     private String addressCity;
+    private String addressPostcode;
+    private String addressState;
     private String addressCountry;
 
     public String getNumber() {
@@ -66,6 +69,15 @@ public class PinCard implements Serializable {
         this.addressLine1 = addressLine1;
     }
 
+    @XmlElement(name="address_line2")
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+    
     @XmlElement(name="address_city")
     public String getAddressCity() {
         return addressCity;
@@ -75,6 +87,24 @@ public class PinCard implements Serializable {
         this.addressCity = addressCity;
     }
 
+    @XmlElement(name="address_postcode")
+    public String getAddressPostcode() {
+        return addressPostcode;
+    }
+
+    public void setAddressPostcode(String addressPostcode) {
+        this.addressPostcode = addressPostcode;
+    }
+
+    @XmlElement(name="address_state")
+    public String getAddressState() {
+        return addressState;
+    }
+
+    public void setAddressState(String addressState) {
+        this.addressState = addressState;
+    }
+    
     @XmlElement(name="address_country")
     public String getAddressCountry() {
         return addressCountry;
