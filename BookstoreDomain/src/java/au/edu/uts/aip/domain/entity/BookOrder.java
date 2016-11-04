f5ecd2d89cc9203a344adfb7cfd1f0f0729dd0ba
tanman- <tanman-@users.noreply.github.com>
2016-11-05 01:49:43 +1100

    Add additional fields to BookOrder

--- BookstoreDomain/src/java/au/edu/uts/aip/domain/entity/BookOrder.java
+++ BookstoreDomain/src/java/au/edu/uts/aip/domain/entity/BookOrder.java
@@ -1,21 +1,25 @@
 package au.edu.uts.aip.domain.entity;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 /**
  * Note: 'Order' is a SQL-reserved keyword.
- * @author tanman
  */
 @Entity
 public class BookOrder implements Serializable {
 
     private Long id;
     private List<BookOrderLine> orderLines;
+    private double shippingCost;
+    private Date timestamp;
 
     @Id
     @GeneratedValue
@@ -23,16 +27,33 @@ public class BookOrder implements Serializable {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     @OneToMany
     public List<BookOrderLine> getOrderLines() {
         return orderLines;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public void setOrderLines(List<BookOrderLine> orderLines) {
         this.orderLines = orderLines;
+    }
+
+    public double getShippingCost() {
+        return shippingCost;
+    }
+
+    public void setShippingCost(double shippingCost) {
+        this.shippingCost = shippingCost;
+    }
+
+    @Temporal(TemporalType.TIMESTAMP)
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
     }
 }
