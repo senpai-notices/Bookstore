83d302e06ebd4df2458ac9c1368eaa1588b0eed5
liuxy900211 <liuxy900211@172.16.53.169>
2016-11-05 19:47:44 +0800

    added some new comments to the code in Category class

--- BookstoreDomain/src/java/au/edu/uts/aip/domain/entity/Category.java
+++ BookstoreDomain/src/java/au/edu/uts/aip/domain/entity/Category.java
@@ -1,5 +1,8 @@
 package au.edu.uts.aip.domain.entity;
 
+/**
+ *The needed libraries
+ */
 import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,15 +10,36 @@ import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 
+
+/**
+ * A data model class to access data in the Category table in the database. 
+ * A Category object describes information of book category
+ *
+ * Category entity has properties id and category name
+ * All the attributes of this model class are private attributes and all of them have relative get and set functions in
+ * order to access the attributes
+ *
+ * @author
+ */
 @Entity
 @NamedQueries({
     @NamedQuery(name = "Category.find", query = "SELECT c from Category c where c.categoryName=:name")
 })
 public class Category implements Serializable {
-
+    /**
+     * the id of category
+     */
     private int Id;
+    /**
+     * the name of the category
+     */
     private String categoryName;
 
+    /**
+     * {@link Category#id}
+     * data type: Integer
+     * return an int value
+     */
     @Id
     @GeneratedValue
     public int getId() {
@@ -26,6 +50,11 @@ public class Category implements Serializable {
         this.Id = Id;
     }
 
+     /**
+     * {@link Category#categoryName}
+     * data type: String
+     * return a String value
+     */
     public String getCategoryName() {
         return categoryName;
     }
