156b31cbfa7d9aaf5dd93ee2fe86385f4b36350d
liuxy900211 <liuxy900211@172.16.53.169>
2016-11-06 15:14:29 +0800

    added new comments to the code in AdminBean and Suburb classes

--- BookstoreDomain/src/java/au/edu/uts/aip/domain/entity/Suburb.java
+++ BookstoreDomain/src/java/au/edu/uts/aip/domain/entity/Suburb.java
@@ -1,5 +1,8 @@
 package au.edu.uts.aip.domain.entity;
 
+/**
+ *The needed libraries
+ */
 import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -8,15 +11,44 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+/**
+ * Suburb is an Entity that is used to access the data in Suburb table in the database
+ * It is a model class
+ * 
+ * It has properties: 
+ * id: Long value
+ * name: String value
+ * stateName: String value
+ * postcode: Integer value
+ * The attributes are not null and accessed by get and set methods
+ * 
+ *  @author team San Dang, Alex Tan, Xiaoyang Liu
+ */
 @Entity
 @Table(name = "suburb")
 public class Suburb implements Serializable {
-
+    /**
+     * the id of the suburb record
+     */
     private Long id;
+    /**
+     * the name of the suburb
+     */
     private String name;
+    /**
+     * the state name of the suburb
+     */
     private String stateName;
+    /**
+     * the postcode of the suburb
+     */
     private int postcode;
 
+    /**
+    * {@link Suburb#id}
+    * data type: Long
+    * return an long Value
+    */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     public Long getId() {
@@ -27,6 +59,11 @@ public class Suburb implements Serializable {
         this.id = id;
     }
 
+    /**
+    * {@link Suburb#name}
+    * data type: String
+    * return an String Value
+    */
     @Column(nullable = false, name = "name")
     public String getName() {
         return name;
@@ -36,6 +73,11 @@ public class Suburb implements Serializable {
         this.name = name;
     }
 
+    /**
+    * {@link Suburb#stateName}
+    * data type: String
+    * return an String Value
+    */
     @Column(nullable = false, name = "state_name")
     public String getStateName() {
         return stateName;
@@ -45,6 +87,11 @@ public class Suburb implements Serializable {
         this.stateName = stateName;
     }
 
+    /**
+    * {@link Suburb#postcode}
+    * data type: Integer
+    * return an int Value
+    */
     @Column(nullable = false, name = "postcode")
     public int getPostcode() {
         return postcode;
