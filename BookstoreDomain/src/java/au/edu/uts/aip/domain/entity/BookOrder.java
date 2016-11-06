a4e5f982f204f4a83252fe196bace3322d5251aa
tanman- <tanman-@users.noreply.github.com>
2016-11-07 01:05:39 +1100

    Edit comment

--- BookstoreDomain/src/java/au/edu/uts/aip/domain/entity/BookOrder.java
+++ BookstoreDomain/src/java/au/edu/uts/aip/domain/entity/BookOrder.java
@@ -14,12 +14,11 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 /**
- * Note: 'Order' is a SQL-reserved keyword.
  * A Data model class to access data in the BookOrder table. BookOrder describes the order item list
  * It has properties Id, orderLines, postageCost and orderTimestamp
  * All the attributes of this model class are private attributes and all of them have relative get and set functions in
  * order to access the attributes
- *
+ * Note: 'Order' is a reserved keyword and is therefore not used.
  */
 @Entity
 public class BookOrder implements Serializable {
