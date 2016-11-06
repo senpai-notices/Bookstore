d71456baf6860991572cc2acb2812926a84af5e5
liuxy900211 <liuxy900211@172.16.53.169>
2016-11-06 09:37:11 +0800

    added new comments to the code in AddressDTO class

--- BookstoreDomain/src/java/au/edu/uts/aip/domain/dto/AddressDTO.java
+++ BookstoreDomain/src/java/au/edu/uts/aip/domain/dto/AddressDTO.java
@@ -1,17 +1,53 @@
 package au.edu.uts.aip.domain.dto;
 
+/**
+ *The needed libraries
+ */
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+/**
+ * A AddressDTO is a DTO class that is used to access all the information that is related to a address
+ * It is a data transfer object for retrieving the address information
+ * describes the detail of the address of the user
+ * It has properties: addressLine1, addressLine2, addressCity, addressCountry, addressPostcode and addressState
+ * 
+ *  @author team San Dang, Alex Tan, Xiaoyang Liu
+ */
 @XmlRootElement
 public class AddressDTO {
+    
+    /**
+     * the first line of the address
+     */
     private String addressLine1;
+    /**
+     * the second line of the address
+     */
     private String addressLine2;
+    /**
+     * the city of the address
+     */
     private String addressCity;
+    /**
+     * the country of the address
+     */
     private String addressCountry;
+    /**
+     * the postcode of the address
+     */
     private int addressPostCode;
+    /**
+     * the state of the address
+     */
     private String addressState;
 
+    
+    /**
+    * {@link AddressDTO#addressLine1}
+    * data type: String
+    * return a String value
+    */
     @XmlElement(name = "address_line1")
     public String getAddressLine1() {
         return addressLine1;
@@ -21,6 +57,11 @@ public class AddressDTO {
         this.addressLine1 = addressLine1;
     }
 
+    /**
+    * {@link AddressDTO#addressLine2}
+    * data type: String
+    * return a String value
+    */
     @XmlElement(name = "address_line2")
     public String getAddressLine2() {
         return addressLine2;
@@ -30,6 +71,11 @@ public class AddressDTO {
         this.addressLine2 = addressLine2;
     }
 
+    /**
+    * {@link AddressDTO#addressCity}
+    * data type: String
+    * return a String value
+    */
     @XmlElement(name = "address_city")
     public String getAddressCity() {
         return addressCity;
@@ -39,6 +85,11 @@ public class AddressDTO {
         this.addressCity = addressCity;
     }
     
+    /**
+    * {@link AddressDTO#addressCountry}
+    * data type: String
+    * return a String value
+    */
     @XmlElement(name = "address_country")
     public String getAddressCountry() {
         return addressCountry;
@@ -48,6 +99,11 @@ public class AddressDTO {
         this.addressCountry = addressCountry;
     }
 
+    /**
+    * {@link AddressDTO#addressPostcode}
+    * data type: Integer
+    * return an int value
+    */
     @XmlElement(name = "address_postcode")
     public int getAddressPostCode() {
         return addressPostCode;
@@ -57,6 +113,11 @@ public class AddressDTO {
         this.addressPostCode = addressPostCode;
     }
 
+    /**
+    * {@link AddressDTO#addressState}
+    * data type: String
+    * return a String value
+    */
     @XmlElement(name = "address_state")
     public String getAddressState() {
         return addressState;
