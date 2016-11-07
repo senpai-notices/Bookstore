7120641ec7b977ad4c9bb4bb078550b8faffbc89
liuxy900211 <liuxy900211@172.16.53.169>
2016-11-07 11:33:52 +0800

    added new documentation to the code in classes ActivationException, PasswordResetException and TokenGenerationException

--- BookstoreDomain/src/java/au/edu/uts/aip/domain/exception/ActivationException.java
+++ BookstoreDomain/src/java/au/edu/uts/aip/domain/exception/ActivationException.java
@@ -1,20 +1,20 @@
 package au.edu.uts.aip.domain.exception;
 
 /**
- *
+ * The ActivatinException class is used to activate the exception
  * @author tanman
  */
 public class ActivationException extends Exception {
 
     /**
-     *
+     *the constructor of the class
      */
     public ActivationException() {
 
     }
 
     /**
-     *
+     * activate the exception
      * @param message
      */
     public ActivationException(String message) {
