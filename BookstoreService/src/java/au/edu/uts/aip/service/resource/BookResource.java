29965061c8c5e8649851d0b3f4399f9b4cb1ccb8
tanman- <tanman-@users.noreply.github.com>
2016-11-07 14:06:23 +1100

    Split BookstoreBean into BookBean and BookSaleBean

--- BookstoreService/src/java/au/edu/uts/aip/service/resource/BookResource.java
+++ BookstoreService/src/java/au/edu/uts/aip/service/resource/BookResource.java
@@ -1,6 +1,5 @@
 package au.edu.uts.aip.service.resource;
 
-import au.edu.uts.aip.domain.remote.BookstoreRemote;
 import au.edu.uts.aip.domain.dto.BookDTO;
 import au.edu.uts.aip.domain.dto.BookSaleDTO;
 import java.util.Arrays;
@@ -18,22 +17,29 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
+import au.edu.uts.aip.domain.remote.BookRemote;
+import au.edu.uts.aip.domain.remote.BookSaleRemote;
 
 /**
  * REST endpoint for book methods
+ *
  * @author Son Dang, Alex Tan, Xiaoyang Liu
  */
 @Path("book")
 public class BookResource {
 
     @EJB
-    private BookstoreRemote bookstoreBean;
+    private BookRemote bookBean;
+
+    @EJB
+    private BookSaleRemote bookSaleBean;
 
     @Context
     private SecurityContext securityContext;
 
     /**
      * Get latest books
+     *
      * @param offset
      * @param limit
      * @return
@@ -44,12 +50,13 @@ public class BookResource {
     public Response getLatestBooks(@QueryParam("offset") int offset,
             @QueryParam("limit") int limit) {
 
-        List<BookDTO> booksDTO = bookstoreBean.getLatestBooks(offset, limit);
+        List<BookDTO> booksDTO = bookBean.getLatestBooks(offset, limit);
         return Response.status(Response.Status.OK).entity(booksDTO.toArray(new BookDTO[0])).build();
     }
 
     /**
      * Get books by ISBN10, ISBN13 and/or title
+     *
      * @param isbn10
      * @param isbn13
      * @param title
@@ -60,13 +67,14 @@ public class BookResource {
     public Response getBook(@QueryParam("isbn10") String isbn10,
             @QueryParam("isbn13") String isbn13,
             @QueryParam("title") String title) {
-        BookDTO bookDTO = bookstoreBean.getSingleBook(isbn10, isbn13, title);
+        BookDTO bookDTO = bookBean.getSingleBook(isbn10, isbn13, title);
 
         return Response.ok(bookDTO).build();
     }
 
     /**
      * Update a sale with its newer object
+     *
      * @param salesData
      * @return
      */
@@ -76,12 +84,13 @@ public class BookResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response updateSales(BookDTO salesData) {
-        BookDTO result = bookstoreBean.updateSale(securityContext.getUserPrincipal().getName(), salesData);
+        BookDTO result = bookSaleBean.updateSale(securityContext.getUserPrincipal().getName(), salesData);
         return Response.ok().entity(result).build();
     }
 
     /**
      * Get particular sales of books by its ID/s
+     *
      * @param saleIds
      * @return
      */
@@ -89,7 +98,7 @@ public class BookResource {
     @Path("sales")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getSales(@QueryParam("saleIds") String saleIds) {
-        List<BookSaleDTO> result = bookstoreBean.getSales(Arrays.asList(saleIds.split(",")).stream().map(Long::parseLong).collect(Collectors.toList()));
+        List<BookSaleDTO> result = bookSaleBean.getSales(Arrays.asList(saleIds.split(",")).stream().map(Long::parseLong).collect(Collectors.toList()));
         return Response.ok().entity(result.toArray(new BookSaleDTO[0])).build();
     }
 }
