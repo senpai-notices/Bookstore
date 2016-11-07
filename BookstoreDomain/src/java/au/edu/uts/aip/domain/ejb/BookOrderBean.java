b4094ec4a29696ae75b2d1516c12dec959442ff8
tanman- <tanman-@users.noreply.github.com>
2016-11-07 12:00:44 +1100

    Docn BookOrderRemote

--- BookstoreDomain/src/java/au/edu/uts/aip/domain/ejb/BookOrderBean.java
+++ BookstoreDomain/src/java/au/edu/uts/aip/domain/ejb/BookOrderBean.java
@@ -23,53 +23,59 @@ import javax.persistence.TypedQuery;
 import javax.ws.rs.ClientErrorException;
 import javax.ws.rs.core.Response;
 
+/**
+ * @inheritDoc
+ */
 @Stateless
 public class BookOrderBean implements BookOrderRemote {
-    
+
     @EJB
     private PaymentBean paymentBean;
-    
+
     @EJB
     private UserBean userBean;
-    
+
     @PersistenceContext
     private EntityManager em;
-    
+
+    /**
+     * @inheritDoc
+     */
     @Override
-    public void checkout(CheckoutDTO checkoutDTO, String username){
+    public void checkout(CheckoutDTO checkoutDTO, String username) {
         User user = userBean.getUserEntity(username);
-        
+
         // create orders
         List<Long> saleIds = checkoutDTO.getItems().stream().map(CheckoutItemDTO::getSaleId).collect(Collectors.toList());
         TypedQuery<BookSales> typedQuery = em.createNamedQuery("BookSales.findSalesByIds", BookSales.class);
         typedQuery.setParameter("saleIds", saleIds);
         List<BookSales> bookSales = typedQuery.getResultList();
-        
+
         // generate order line from each bookSale
         List<BookOrderLine> orderLineList = new ArrayList<>();
-        for(BookSales bookSale: bookSales){
+        for (BookSales bookSale : bookSales) {
             CheckoutItemDTO matchedItem = null;
-            for (CheckoutItemDTO item: checkoutDTO.getItems()){
-                if (bookSale.getSalesId() == item.getSaleId()){
+            for (CheckoutItemDTO item : checkoutDTO.getItems()) {
+                if (bookSale.getSalesId() == item.getSaleId()) {
                     matchedItem = item;
                     break;
                 }
             }
-            
-            if (matchedItem == null){
+
+            if (matchedItem == null) {
                 throw new RuntimeException("Sales of book " + bookSale.getBook().getTitle()
-                                            + " from seller " + bookSale.getSeller().getUsername()
-                                            + " is not exist");
+                        + " from seller " + bookSale.getSeller().getUsername()
+                        + " is not exist");
             }
-            
-            if (bookSale.getQuantity() < matchedItem.getBuyQuantity()){
+
+            if (bookSale.getQuantity() < matchedItem.getBuyQuantity()) {
                 throw new RuntimeException("Sales of book " + bookSale.getBook().getTitle()
-                                            + " from seller " + bookSale.getSeller().getUsername()
-                                            + " only have " + bookSale.getQuantity() + " books remains");
+                        + " from seller " + bookSale.getSeller().getUsername()
+                        + " only have " + bookSale.getQuantity() + " books remains");
             }
-            
+
             bookSale.setQuantity(bookSale.getQuantity() - matchedItem.getBuyQuantity());
-            
+
             BookOrderLine orderLine = new BookOrderLine();
             orderLine.setBook(bookSale.getBook());
             orderLine.setSeller(bookSale.getSeller());
@@ -80,7 +86,7 @@ public class BookOrderBean implements BookOrderRemote {
             em.persist(orderLine);
             orderLineList.add(orderLine);
         }
-        
+
         // add order lines to order
         BookOrder bookOrder = new BookOrder();
         bookOrder.setOrderLines(orderLineList);
@@ -88,54 +94,57 @@ public class BookOrderBean implements BookOrderRemote {
         bookOrder.setPostageCost(checkoutDTO.getShippingCost());
         bookOrder.setOwner(user);
         em.persist(bookOrder);
-        
+
         // create customer from card detail
         PinCustomerPost pinCustomerPost = new PinCustomerPost();
         pinCustomerPost.setEmail(user.getEmail());
         pinCustomerPost.setCard(checkoutDTO.getCard());
-        
+
         SerialResponse createCustomerResponse = paymentBean.createCustomer(pinCustomerPost);
-        
+
         // failed at create customer
-        if (createCustomerResponse.getStatusCode() != Response.Status.CREATED.getStatusCode()){
+        if (createCustomerResponse.getStatusCode() != Response.Status.CREATED.getStatusCode()) {
             throw new ClientErrorException(createCustomerResponse.getBody().toString(), createCustomerResponse.getStatusCode());
         }
-        
+
         // calculate total cost & charge money
         double totalCost = 0;
-        for(BookOrderLine orderLine: orderLineList){
+        for (BookOrderLine orderLine : orderLineList) {
             totalCost += orderLine.getQuantity() * orderLine.getUnitPrice();
         }
         totalCost += checkoutDTO.getShippingCost();
         String description = "Order " + bookOrder.getId();
         String customerToken = createCustomerResponse.getBody().getJsonObject("response").getString("token");
-        
+
         PinChargePost pinChargePost = new PinChargePost();
-        pinChargePost.setAmount( (int)(totalCost * 100) + "") ;
+        pinChargePost.setAmount((int) (totalCost * 100) + "");
         pinChargePost.setCustomerToken(customerToken);
         pinChargePost.setDescription(description);
         pinChargePost.setEmail(user.getEmail());
-        
+
         SerialResponse chargeResponse = paymentBean.charge(pinChargePost);
         // failed at charge
-        if (chargeResponse.getStatusCode() != Response.Status.CREATED.getStatusCode()){
+        if (chargeResponse.getStatusCode() != Response.Status.CREATED.getStatusCode()) {
             throw new ClientErrorException(chargeResponse.getBody().toString(), chargeResponse.getStatusCode());
         }
     }
-    
+
+    /**
+     * @inheritDoc
+     */
     @Override
     public List<BookOrderDTO> getBuyOrder(String username) {
         User user = userBean.getUserEntity(username);
-        
+
         TypedQuery<BookOrder> typedQuery = em.createNamedQuery("BookOrder.findByOwner", BookOrder.class);
         typedQuery.setParameter("owner", user);
         List<BookOrder> bookOrdersEntity = typedQuery.getResultList();
-        
+
         List<BookOrderDTO> bookOrdersDTO = new ArrayList<>();
         bookOrdersEntity.stream().forEach(bookOrder -> {
             bookOrdersDTO.add(new BookOrderDTO(bookOrder));
         });
-        
+
         return bookOrdersDTO;
     }
 }
